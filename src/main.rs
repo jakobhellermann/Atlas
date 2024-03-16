30f946eadc4f8dc6e9103f619fbd4bf11948caa3
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-03-16 12:55:10 +0100

    read recordings on change (notify)

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,17 @@ use celesterender::{
 };
 use chrono::DateTime;
 use indexmap::IndexMap;
-use notify::Watcher;
-use slint::{Model, SharedString, VecModel};
-use std::{collections::HashSet, path::PathBuf, rc::Rc};
+use notify_debouncer_full::{
+    notify::{self, RecommendedWatcher, Watcher},
+    DebounceEventResult, Debouncer, FileIdMap,
+};
+use slint::{Model, SharedString, VecModel, Weak};
+use std::{
+    collections::HashSet,
+    path::PathBuf,
+    rc::Rc,
+    time::{Duration, Instant},
+};
 
 slint::include_modules!();
 
@@ -26,31 +34,8 @@ pub fn main() -> Result<()> {
     let main_window = MainWindow::new().unwrap();
     main_window.set_recordings_model(recordings.clone().into());
 
-    let watcher_handle = main_window.as_weak().clone();
+    let _watcher = start_watcher(&physics_inspector, main_window.as_weak())?;
 
-    {
-        let recent_recordings_path = physics_inspector.recent_recordings.clone();
-        let physics_inspector = physics_inspector.clone();
-        let mut watcher = notify::recommended_watcher(move |_e| {
-            let _physics_inspector = &physics_inspector;
-            watcher_handle
-                .upgrade_in_event_loop(move |_handle| {
-                    /*let model = handle.get_recordings_model();
-                    match read_recordings(&physics_inspector) {
-                        Err(e) => handle.set_error(format!("{e:?}").into()),
-                        Ok(new) => model
-                            .as_any()
-                            .downcast_ref::<VecModel<CCTRecording>>()
-                            .unwrap()
-                            .set_vec(new),
-                    }
-                    let model = handle.get_recordings_model();
-                    read_recordings(physics_inspector);*/
-                })
-                .unwrap();
-        })?;
-        watcher.watch(&recent_recordings_path, notify::RecursiveMode::NonRecursive)?;
-    }
     // callbacks
     main_window.on_render({
         let recordings = recordings.clone();
@@ -333,4 +318,74 @@ fn read_recordings(physics_inspector: &PhysicsInspector) -> Result<Vec<CCTRecord
             }
         })
         .collect())
+}
+
+fn start_watcher(
+    physics_inspector: &PhysicsInspector,
+    watcher_handle: Weak<MainWindow>,
+) -> Result<Debouncer<RecommendedWatcher, FileIdMap>> {
+    let recent_recordings_path = physics_inspector.recent_recordings.clone();
+    let physics_inspector = physics_inspector.clone();
+
+    let mut last_event = Instant::now();
+
+    let mut debouncer = notify_debouncer_full::new_debouncer(
+        Duration::from_millis(200),
+        None,
+        move |event: DebounceEventResult| {
+            let Ok(event) = event else { return };
+
+            let room_layout_changed =
+                event
+                    .iter()
+                    .flat_map(|event| &event.event.paths)
+                    .any(|path| {
+                        path.to_str()
+                            .map_or(false, |e| e.ends_with("_room-layout.json"))
+                    });
+
+            if room_layout_changed {
+                /*for event in &event {
+                    for path in &event.event.paths {
+                        println!("{:?} {}", event.kind, path.display());
+                    }
+                }*/
+                let now = Instant::now();
+                let since_last = now.duration_since(last_event);
+                last_event = now;
+
+                let physics_inspector = physics_inspector.clone();
+                let result = watcher_handle.upgrade_in_event_loop(move |handle| {
+                    let start_reading = Instant::now();
+                    let model = handle.get_recordings_model();
+                    match read_recordings(&physics_inspector) {
+                        Err(e) => handle.set_error(format!("{e:?}").into()),
+                        Ok(new) => model
+                            .as_any()
+                            .downcast_ref::<VecModel<CCTRecording>>()
+                            .unwrap()
+                            .set_vec(new),
+                    }
+
+                    println!(
+                        "reloading room layouts, {:.02}s after last, took {}ms",
+                        since_last.as_secs_f32(),
+                        start_reading.elapsed().as_secs_f32() / 1000.,
+                    );
+                });
+
+                if let Err(e) = result {
+                    eprintln!("failed to reload room layouts: {e}");
+                }
+            }
+        },
+    )?;
+    debouncer
+        .watcher()
+        .watch(&recent_recordings_path, notify::RecursiveMode::NonRecursive)?;
+    debouncer
+        .cache()
+        .add_root(&recent_recordings_path, notify::RecursiveMode::NonRecursive);
+
+    Ok(debouncer)
 }
